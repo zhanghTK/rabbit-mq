9e944e0ccb221e0fc92ce1d96208fe520b1039fc
zhanghTk <510223064@qq.com>
2017-08-16 20:52:51 +0800

    add rabbit amqp demo

---
+++ amqp/src/main/java/tk/zhangh/rabbitmq/tut3/Tut3Sender.java
@@ -0,0 +1,36 @@
+package tk.zhangh.rabbitmq.tut3;
+
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Created by ZhangHao on 2017/8/15.
+ */
+public class Tut3Sender {
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private FanoutExchange fanout;
+
+    int dots = 0;
+
+    int count = 0;
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void send() {
+        StringBuilder builder = new StringBuilder("Hello");
+        if (dots++ == 3) {
+            dots = 1;
+        }
+        for (int i = 0; i < dots; i++) {
+            builder.append('.');
+        }
+        builder.append(Integer.toString(++count));
+        String message = builder.toString();
+        template.convertAndSend(fanout.getName(), "", message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
---
+++ amqp/src/main/java/tk/zhangh/rabbitmq/tut4/Tut4Sender.java
@@ -0,0 +1,37 @@
+package tk.zhangh.rabbitmq.tut4;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Created by ZhangHao on 2017/8/16.
+ */
+public class Tut4Sender {
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private DirectExchange direct;
+
+    private int index;
+
+    private int count;
+
+    private final String[] keys = {"orange", "black", "green"};
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void send() {
+        StringBuilder builder = new StringBuilder("Hello to ");
+        if (++this.index == 3) {
+            this.index = 0;
+        }
+        String key = keys[this.index];
+        builder.append(key).append(' ');
+        builder.append(Integer.toString(++this.count));
+        String message = builder.toString();
+        template.convertAndSend(direct.getName(), key, message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
