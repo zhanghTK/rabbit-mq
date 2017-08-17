8fb744bec15b70c641516fa1c5deab73259a6704
张皓 <zhanghao@cvte.com>
2017-08-17 19:52:50 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rabbit MQ Demo
+
+## amqp
+
+Rabbit MQ with Spring AMQP demo
+
+## java
+
+Rabbit MQ with Java demo
--- src/main/java/tk/zhangh/rebbitmq/java/RabbitConfig.java
+++
@@ -1,9 +0,0 @@
-package tk.zhangh.rebbitmq.java;
-
-/**
- * Created by ZhangHao on 2017/6/10.
- */
-public interface RabbitConfig {
-    // todo
-    String HOST = "";
-}
